0981a65c3cbe0ca0c0809c1a8e631f7d154cb1d9
loveing <483770740@qq.com>
2022-08-28 19:25:29 +0800

    提交代码为了演示GitHub的使用

---
+++ test_2022_8_10/test.c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//	int w = 0;
+//	scanf("%d", &w);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int b = a >> 1;
+//	printf("%d", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a ^ b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	printf("%d%d", a, b);
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("%d%d", a, b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	int count = 0;//计数
+//	while (num)
+//	{
+//		if (num % 2 == 1)
+//			count++;
+//		num = num / 2;
+//	}
+//	printf("二进制中1的个数=%d\n", count);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	int count = 0;
+//	int i;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (1 == ((num >> i) & 1))
+//		{
+//			count++;
+//		}
+//	}
+//	printf("二进制中1的个数=%d", count);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	int i = 0;
+//	int count = 0;
+//	while (num)
+//	{
+//		count++;
+//		num = num & (num - 1);
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+/*num= 011
+ num-1=010
+ num=  010
+*/
+//
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;//取地址操作符
+//	*p = 20;//解引用操作符
+//	return 0;
+//}
+
